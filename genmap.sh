a3caafc902c3679655e31ec14fcaacd0413889ec
Brian Mayer <bleemayer@gmail.com>
2022-01-14 09:35:59 -0300

    Added main script

---
+++ genmap.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/sh
+
+version() {
+    echo "genmap v1.0.0"
+}
+
+usage() {
+    cat <<EOF
+genmap v1.0.0
+Usage: genmap [options] [path]
+genmap is a tool to generate sitemap.txt files
+
+Available options are:
+  -h		show this help
+  -v		print the version
+  -d		set the domain
+
+Examples:
+  genmap -d example.com out/ > sitemap.txt
+
+MIT License
+
+Copyright (c) 2021 Brian Mayer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+EOF
+}
+
+# Command line options
+while getopts "d:hv" name
+do
+    case $name in
+    d) DOMAIN="$OPTARG" && shift && shift;;
+    h) usage && exit 0 ;;
+    v) version && exit 0 ;;
+    ?) usage && exit 1 ;;
+    *) args+="$name" ;;
+    esac
+done
+
+[ -z "$DOMAIN" ] && echo "missing domain" && usage && exit 1
+
+find "$@" -type f -printf "$DOMAIN/%P\n"
